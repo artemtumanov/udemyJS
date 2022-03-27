79a0ee86eea4042828b8d314c316d6630dbcdb34
artem <artem.tumanov.93@inbox.ru>
2022-03-27 19:43:03 +0300

    add lesson 4

--- projectJS/js/script3.js
+++ projectJS/js/script3.js
@@ -33,33 +33,33 @@ let personalMovieDB = {
     privat: false
 };
 
-// function rememberMyFilms() {
-//     for (let i = 0; i < 2; i++) {
-//         let a = prompt('Один из последних просмотренных фильмов?', '');
-//         let b = prompt('На сколько оцените его?', '');
-//         if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-//             personalMovieDB.movies[a] = b;
-//         } else {
-//             i--;
-//         }
-//     }
-// }
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        let a = prompt('Один из последних просмотренных фильмов?', '');
+        let b = prompt('На сколько оцените его?', '');
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+        } else {
+            i--;
+        }
+    }
+}
 
-// rememberMyFilms();
+rememberMyFilms();
 
-// function detectPersonalLevel() {
-//     if (personalMovieDB.count < 10) {
-//         console.log ('Просмотрено довольно мало фильмов');
-//     } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-//         console.log ('Вы классический зритель');
-//     } else if (personalMovieDB.count >= 30) {
-//         console.log ('Вы киноман');
-//     }else {
-//         console.log('Произошла ошибка');
-//     }
-// }
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log ('Просмотрено довольно мало фильмов');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log ('Вы классический зритель');
+    } else if (personalMovieDB.count >= 30) {
+        console.log ('Вы киноман');
+    }else {
+        console.log('Произошла ошибка');
+    }
+}
 
-// detectPersonalLevel();
+detectPersonalLevel();
 
 function showMyDB() {
     if (personalMovieDB.privat == false) {
