9362ccbcf5b17254670485bfb11f67ed101697bc
artem <artem.tumanov.93@inbox.ru>
2022-04-11 18:09:50 +0300

    add lesson 33

--- project/js/script.js
+++ project/js/script.js
@@ -20,14 +20,17 @@ const movieDB = {
         "Лига справедливости",
         "Ла-ла лэнд",
         "Одержимость",
-        "Скотт Пилигрим против..."
+        "Скотт Пилигрим против...",
     ]
 };
 
 const adv = document.querySelectorAll('.promo__adv > img'),
       genre = document.querySelector('.promo__genre'),
       bg = document.querySelector('.promo__bg'),
-      moviesList = document.querySelector('.promo__interactive-list');
+      moviesList = document.querySelector('.promo__interactive-list'),
+      btn = document.querySelector('.button'),
+      input = document.querySelector('.adding__input'),
+      checkbox = document.querySelector('.checkbox');
 
 adv.forEach(function(item) {
    item.remove();
@@ -39,15 +42,64 @@ bg.style.background = 'url("img/bg.jpg") center center/cover no-repeat';
 
 movieDB.movies.sort();
 
-moviesList.innerHTML = '';
+addNewMovie();
 
-movieDB.movies.forEach((item, i) => {
-    moviesList.innerHTML += `
-        <li class="promo__interactive-item"> ${i + 1}. ${item}
-            <div class="delete"></div>
-        </li>
-    `;
+/* Задания на урок:
+
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
+
+2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
+
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
+
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+"Добавляем любимый фильм"
+
+5) Фильмы должны быть отсортированы по алфавиту */
+
+
+function addNewMovie () {
+    moviesList.innerHTML = '';
+    movieDB.movies.forEach((item, i) => {
+        if (item.length > 21) {
+            item = item.substring(0, 21) + '...';
+        }
+        moviesList.innerHTML += `
+            <li class="promo__interactive-item"> ${i + 1}. ${item}
+                <div class="delete"></div>
+            </li>
+        `;
+    });
+    document.querySelectorAll('.delete').forEach((btn, i) => {
+        btn.addEventListener('click', () => {
+            btn.parentElement.remove();
+            movieDB.movies.splice(i, 1);
+
+            addNewMovie();
+        })
+    });
+}
+
+btn.addEventListener('click', () => {
+    if (input.value) {
+        movieDB.movies.push(input.value);
+        movieDB.movies.sort();
+        addNewMovie();
+        if (checkbox.checked) {
+            console.log('Добавляем любимый фильм');
+        };
+    }
+    input.value = '';
 });
+
+
+
+
+
 
 
 
